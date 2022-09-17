b28128a1f57ec6bb187137e2a38ed8c03e68f615
Vu Ngoc Phuoc <phuocvungoc9997@gmail.com>
2022-09-17 08:45:16 +0700

    Lab5.1 Chuyển đổi tệp HTML sang Pug

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const express = require("express");
 const path = require("path");
 
-const mainRoutes = require("./routes/index");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(mainRoutes);
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
 
 app.listen(3000);
